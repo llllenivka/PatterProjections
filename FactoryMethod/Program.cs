418c37d235fecb98318ca02c46ec1fd27478027d
Adelina <adelisha.dinov@gmail.com>
2024-12-18 14:13:47 +0300

    add: create variable creator

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -2,36 +2,44 @@
 {
     static void Main()
     {
+        NotificationCreator ? creator = null;
+        NewNotification(ref creator);
+        if(creator != null) {
+            INotification notification = creator.CreatNotification();
+            notification.Send();
+        }
+    }
+
+    static void NewNotification(ref NotificationCreator ? creator)
+    {    
         bool exit = false;
-        NotificationCreator creator;
         while(!exit)
         {
+            exit = true;
+
             Console.WriteLine("Отправить уведомление через:");
             Console.WriteLine("1 - Email");
             Console.WriteLine("2 - SMS");
             Console.WriteLine("3 - Push");
-            Console.WriteLine("4 - Exit");
 
             string ? input = Console.ReadLine();
             switch (input)
             {
                 case "1":
-                    // creator = new
+                    creator = new EmailNotificationCreator();
                     break;
                 case "2":
+                    creator = new SMShNotificationCreator();
                     break;
                 case "3":
-                    break;
-                case "4":
-                    exit = true;
+                    creator = new PushNotificationCreator();
                     break;
                 default:
                     Console.WriteLine("Incorrect input. Please, try again");
+                    exit = false;
                     break;
             }
         }
-        
-        
-        
+
     }
 }
